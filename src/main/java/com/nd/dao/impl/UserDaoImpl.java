2a4683e53b8588875cc9fe3d9926df70f78ed1ec
JYOTSNA\Kiran <nextDigits@gmail.com>
2016-11-18 17:44:44 -0600

    Adding user dao impl package

---
+++ src/main/java/com/nd/dao/impl/UserDaoImpl.java
@@ -0,0 +1,13 @@
+package com.nd.dao.impl;
+
+import com.nd.dao.UserDao;
+
+public class UserDaoImpl implements UserDao {
+
+	@Override
+	public void saveUser() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
